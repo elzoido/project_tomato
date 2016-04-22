bb031eae3b8b6333a48cdb816064f5baead512f7
Sebastian Sproesser <elzoido@elbloggo.de>
2016-04-22 22:02:40 +0200

    Added support for Adafruit IOT API

--- project_tomato/project_tomato.ino
+++ project_tomato/project_tomato.ino
@@ -4,10 +4,13 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include "Adafruit_IO_Client.h"
 
 #include <ArduinoJson.h>
 
 #include "config.h"
+
+#define SERIALOUT 1
 
 const int water_pin = 14; // D5 on LoLin board
 const int cnt_sensor = 2;
@@ -17,12 +20,24 @@ const int cnt_sensor = 2;
  * * GPIO4 (D2): SDA
  * * GPIO5 (D1): SCL
  */
+
+WiFiClient client;
+Adafruit_IO_Client aio = Adafruit_IO_Client(client, AIO_KEY);
+
 I2CSoilMoistureSensor sensor[cnt_sensor]; 
 
 int sensor_addr[cnt_sensor];
+
 int last_moisture[cnt_sensor];
 int last_temp[cnt_sensor];
 int last_light[cnt_sensor];
+
+Adafruit_IO_Feed feed_moisture = aio.getFeed("moisture");
+Adafruit_IO_Feed feed_temp = aio.getFeed("temperature");
+Adafruit_IO_Feed feed_light = aio.getFeed("light");
+Adafruit_IO_Feed feed_threshold_min = aio.getFeed("water-threshold-min");
+Adafruit_IO_Feed feed_threshold_max = aio.getFeed("water-threshold-max");
+Adafruit_IO_Feed feed_watering_status = aio.getFeed("current-watering-status");
 
 unsigned int last_poll = 0;
 bool checked_light = 1;
@@ -124,20 +139,25 @@ void setup() {
   digitalWrite(water_pin, 0);
 
   Wire.begin();
-  Serial.begin(9600);
+
+  if (SERIALOUT)
+    Serial.begin(9600);
 
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    if (SERIALOUT)
+      Serial.print(".");
   }
 
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  if (SERIALOUT) {
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
 
   server.on("/", handleRoot);
 
@@ -148,6 +168,8 @@ void setup() {
 
   server.onNotFound(handleNotFound);
 
+  aio.begin();
+
   // Set sensor adresses and initialize
   sensor[0].changeSensor(0x20);
   sensor[1].changeSensor(0x21);
@@ -157,46 +179,66 @@ void setup() {
   sensor2.begin(); // reset sensor
   */
   delay(1000); // give some time to boot up
-  Serial.println("Getting first sensor readings...");
+  if (SERIALOUT)
+    Serial.println("Getting first sensor readings...");
 
   for (int i = 0; i < cnt_sensor; i++)
     sensor_addr[i] = sensor[i].getAddress();
 
   // Start first reading of sensors
-  for (int i = 0; i < cnt_sensor; i++) {
-    Serial.print("Moisture sensor ");
-    Serial.print(i);
-    Serial.print(": ");
-    last_moisture[i] = sensor[i].getCapacitance();
-    Serial.println(last_moisture[i]);
-  }
+  int sum_moisture = 0;
+  int sum_temp = 0;
+  int sum_light = 0;
   
   for (int i = 0; i < cnt_sensor; i++) {
-    Serial.print("Temperature sensor ");
-    Serial.print(i);
-    Serial.print(": ");
-    last_temp[i] = sensor[i].getTemperature();
-    Serial.println(last_temp[i] / (float)10);
+    last_moisture[i] = sensor[i].getCapacitance();
+    sum_moisture += last_moisture[i];
+    if (SERIALOUT) {
+      Serial.print("Moisture sensor ");
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.println(last_moisture[i]);
+    }
   }
+  feed_moisture.send(sum_moisture / cnt_sensor);  
+  
+  for (int i = 0; i < cnt_sensor; i++) {
+    last_temp[i] = sensor[i].getTemperature();
+    sum_temp += last_temp[i];
+    if (SERIALOUT) {
+      Serial.print("Temperature sensor ");
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.println(last_temp[i] / (float)10);
+    }
+  }
+  feed_temp.send(sum_temp / (cnt_sensor * 10.0));
 
-  Serial.println("Start polling light sensors and wait for 3 seconds");
+  if (SERIALOUT)
+    Serial.println("Start polling light sensors and wait for 3 seconds");
   // Start polling light sensors
   for (int i = 0; i < cnt_sensor; i++)
     sensor[i].startMeasureLight();
   delay(3000);
   for (int i = 0; i < cnt_sensor; i++) {
     last_light[i] = sensor[i].getLight();
-    Serial.print("Light sensor ");
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.println(last_light[i]);
+    sum_light += last_light[i];
+    if (SERIALOUT) {
+      Serial.print("Light sensor ");
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.println(last_light[i]);
+    }
   }
+  feed_light.send(sum_light / cnt_sensor);
+  
   checked_light = 1;
 
   last_poll = millis();
 
   server.begin();
-  Serial.println("HTTP server started");
+  if (SERIALOUT)
+    Serial.println("HTTP server started");
 
 }
 
@@ -207,37 +249,61 @@ void loop() {
   unsigned int now = millis();
 
   if (last_poll + (1000 * poll_seconds) < now) {
-    Serial.print(poll_seconds);
-    Serial.println(" seconds are over, let's get data from the sensors");
-
-    int moisture_sum = 0;
-
-    for (int i = 0; i < cnt_sensor; i++) {
-      Serial.print("Moisture sensor ");
-      Serial.print(i);
-      Serial.print(": ");
-      last_moisture[i] = sensor[i].getCapacitance();
-      moisture_sum += last_moisture[i];
-      Serial.println(last_moisture[i]);
+    if (SERIALOUT) {
+      Serial.print(poll_seconds);
+      Serial.println(" seconds are over, let's get data from the sensors");
     }
+
+    int sum_moisture = 0;
+    int sum_temp = 0;
+
+    FeedData min = feed_threshold_min.receive();
+    if (min.isValid()) {
+      min.intValue(&water_threshold_min);
+    }
+    FeedData max = feed_threshold_max.receive();
+    if (max.isValid()) {
+      max.intValue(&water_threshold_max);
+    }
+    
+    for (int i = 0; i < cnt_sensor; i++) {
+      last_moisture[i] = sensor[i].getCapacitance();
+      sum_moisture += last_moisture[i];
+      if (SERIALOUT) {
+        Serial.print("Moisture sensor ");
+        Serial.print(i);
+        Serial.print(": ");
+        Serial.println(last_moisture[i]);
+      }
+    }
+    feed_moisture.send(sum_moisture / cnt_sensor);  
+
 
     // Check if we need to water the soil
-    if (moisture_sum / (float)cnt_sensor < moisture_threshold) {
+    if (sum_moisture / (float)cnt_sensor < water_threshold_min) {
         digitalWrite(water_pin, 1);
-    } else {
+        feed_watering_status.send(1);
+    } else if (sum_moisture / (float)cnt_sensor > water_threshold_max) {
         digitalWrite(water_pin, 0);
-    }
-  
-    for (int i = 0; i < cnt_sensor; i++) {
-      Serial.print("Temperature sensor ");
-      Serial.print(i);
-      Serial.print(": ");
-      last_temp[i] = sensor[i].getTemperature();
-      Serial.println(last_temp[i] / (float)10);
+        feed_watering_status.send(0);
     }
 
+    for (int i = 0; i < cnt_sensor; i++) {
+      last_temp[i] = sensor[i].getTemperature();
+      sum_temp += last_temp[i];
+      if (SERIALOUT) {
+        Serial.print("Temperature sensor ");
+        Serial.print(i);
+        Serial.print(": ");
+        Serial.println(last_temp[i] / (float)10);
+      }
+    }
+    feed_temp.send(sum_temp / (cnt_sensor * 10.0));
+
     // Start polling light sensors
-    Serial.println("Start polling light sensors and wait for 3 seconds");
+    if (SERIALOUT)
+      Serial.println("Start polling light sensors and wait for 3 seconds");
+
     for (int i = 0; i < cnt_sensor; i++)
       sensor[i].startMeasureLight();
 
@@ -247,14 +313,21 @@ void loop() {
 
   if ((!checked_light) && (last_poll + 3000 < now)) {
     // Light is ready to read!
+
+    int sum_light = 0;
+
     for (int i = 0; i < cnt_sensor; i++) {
       last_light[i] = sensor[i].getLight();
-      Serial.print("Light sensor ");
-      Serial.print(i);
-      Serial.print(": ");
-      Serial.println(last_light[i]);
+      sum_light += last_light[i];
+      if (SERIALOUT) {
+        Serial.print("Light sensor ");
+        Serial.print(i);
+        Serial.print(": ");
+        Serial.println(last_light[i]);
+      }
     }
-    
+    feed_light.send(sum_light / cnt_sensor);
+        
     checked_light = 1;
   }
 }
